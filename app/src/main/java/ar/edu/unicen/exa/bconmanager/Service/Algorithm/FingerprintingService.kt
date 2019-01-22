1b96f15d04f8062a066bb02e72161eeef4aa169c
Andres Chardon <chardon.andres@gmail.com>
2019-01-22 16:11:21 -0300

    Fingerprinting try to avoid jumps

--- app/src/main/java/ar/edu/unicen/exa/bconmanager/Service/Algorithm/FingerprintingService.kt
+++ app/src/main/java/ar/edu/unicen/exa/bconmanager/Service/Algorithm/FingerprintingService.kt
@@ -3,11 +3,11 @@ package ar.edu.unicen.exa.bconmanager.Service.Algorithm
 import android.util.Log
 import ar.edu.unicen.exa.bconmanager.Adapters.FingerprintOfflineAdapter
 import ar.edu.unicen.exa.bconmanager.Model.BeaconDevice
-import ar.edu.unicen.exa.bconmanager.Model.CustomMap
 import ar.edu.unicen.exa.bconmanager.Model.FingerprintZone
-import ar.edu.unicen.exa.bconmanager.Service.BluetoothScanner
 import ar.edu.unicen.exa.bconmanager.Model.Json.JsonData
 import ar.edu.unicen.exa.bconmanager.Model.Location
+import ar.edu.unicen.exa.bconmanager.Service.BluetoothScanner
+
 
 class FingerprintingService() : Algorithm(){
 
@@ -20,7 +20,7 @@ class FingerprintingService() : Algorithm(){
         for (b in savedBeacons) {
             sBeacons.add(b.beacon)
         }
-        currentFingerprintingZone = getCurrentZoneInstant(sBeacons)
+        currentFingerprintingZone = getCurrentZone(sBeacons, true)
         Log.d("FINGERP-ZONE", "Is ${currentFingerprintingZone!!.position}")
         return currentFingerprintingZone!!.position
     }
@@ -64,58 +64,51 @@ class FingerprintingService() : Algorithm(){
     /**
      * Returns the best fingerprinting zone according to RSSI values of the beacons
      */
-    fun getCurrentZone(beacons: List<BeaconDevice>): FingerprintZone? {
+    fun getCurrentZone(beacons: List<BeaconDevice>, instant: Boolean = false): FingerprintZone? {
         val fingerprintZones =  customMap.fingerprintZones
         val fingerprintRating = mutableListOf<Double>()
-        Log.d("RATINGS", beacons.toString())
-        Log.d("RATINGS", fingerprintZones.toString())
+        //Log.d("RATINGS", beacons.toString())
+        //Log.d("RATINGS", fingerprintZones.toString())
+        var maxDistance = 0.0
         for (zone in fingerprintZones) {
             // For each fingerprinting zone, calculate the "rating"
             var differenceRating = 0.0
             zone.fingerprints.forEach {
                 val index = beacons.indexOf(BeaconDevice(it.mac, 0, null))
-                if (index != -1) {
-                    val beacon = beacons.get(index)
-                    differenceRating += Math.abs(beacon.averageRssi - it.rssi)
-                }
-            }
-            fingerprintRating.add(differenceRating)
-        }
-        Log.d("RATINGS", fingerprintRating.toString())
-
-        // Get the one with less rating
-        val index = fingerprintRating.indexOf(fingerprintRating.min())
-        Log.d("RATINGS", "$index")
-
-        val bestZone = fingerprintZones.get(index)
-        Log.d("RATINGS", "Best zone is $bestZone")
-
-        return bestZone
-    }
-
-    /**
-     * Returns the best fingerprinting zone according to RSSI values of the beacons
-     */
-    fun getCurrentZoneInstant(beacons: List<BeaconDevice>): FingerprintZone? {
-        val fingerprintZones =  customMap.fingerprintZones
-        val fingerprintRating = mutableListOf<Double>()
-        Log.d("RATINGS", beacons.toString())
-        Log.d("RATINGS", fingerprintZones.toString())
-        for (zone in fingerprintZones) {
-            Log.d("RATINGS", "THIS ZONE IS ${zone.fingerprints}")
-            // For each fingerprinting zone, calculate the "rating"
-            var differenceRating = 0.0
-            zone.fingerprints.forEach {
-                val index = beacons.indexOf(BeaconDevice(it.mac, 0, null))
-                Log.d("RATINGS", "INDEX IS ${index}")
                 if (index != -1) {
                     val beacon = beacons[index]
-                    Log.d("RATINGS", "Found intensity is ${beacon.intensity} and finger zone rssi is ${it.rssi}")
-                    differenceRating += Math.abs(beacon.intensity - it.rssi)
+                    if (instant) {
+                        // Simulation
+                        differenceRating += Math.abs(beacon.intensity - it.rssi)
+                    } else {
+                        // Real time
+                        differenceRating += Math.abs(beacon.averageRssi - it.rssi)
+                    }
+
                 }
             }
             fingerprintRating.add(differenceRating)
+            // To avoid jumps
+            if (currentFingerprintingZone != null) {
+                val thisDist = distanceBetweenZones(currentFingerprintingZone!!, zone)
+                if (thisDist > maxDistance)
+                    maxDistance = thisDist
+            }
         }
+        Log.d("RATINGS", beacons.toString())
+        Log.d("RATINGS", fingerprintRating.toString())
+        Log.d("RATINGS", "MAX DISTANCE IS $maxDistance")
+
+        // Modify ratings to prioritize closer zones
+        if (currentFingerprintingZone != null) {
+            var index = 0
+            fingerprintZones.forEach {
+                fingerprintRating[index] = prioritizeCloserZones(fingerprintRating[index], it, maxDistance)
+                index++
+            }
+
+        }
+
         Log.d("RATINGS", fingerprintRating.toString())
 
         // Get the one with less rating
@@ -123,9 +116,22 @@ class FingerprintingService() : Algorithm(){
         Log.d("RATINGS", "$index")
 
         val bestZone = fingerprintZones.get(index)
-        Log.d("RATINGS", "Best zone is $bestZone")
+        Log.d("RATINGS", "Best zone is $bestZone ${fingerprintRating[index]}")
+
+        currentFingerprintingZone = bestZone
 
         return bestZone
     }
 
+    private fun prioritizeCloserZones(rating: Double, zone: FingerprintZone, maxDistance: Double): Double {
+        val distance = distanceBetweenZones(currentFingerprintingZone!!, zone)
+        var priotitize = ((20 * distance) / maxDistance)
+        Log.d("RATING", "Formula result is $priotitize")
+        var newRating = rating + priotitize
+        return newRating
+    }
+
+    private fun distanceBetweenZones(first: FingerprintZone, second: FingerprintZone) : Double {
+        return Math.sqrt(Math.pow(second.position.x - first.position.x, 2.0) + Math.pow(second.position.y - first.position.y, 2.0))
+    }
 }
